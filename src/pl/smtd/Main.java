a7e593af22bb91fd7fba5cc44a4d27cde2d6b153
Piotr Ponikowski <piopon.github@gmail.com>
2018-09-01 13:56:22 +0200

    calling RecursionExamples from main menu

--- src/pl/smtd/Main.java
+++ src/pl/smtd/Main.java
@@ -1,11 +1,27 @@
 package pl.smtd;
 
+import pl.smtd.menu.ConsoleExercise;
+import pl.smtd.menu.ConsoleMenu;
 import pl.smtd.recursion.RecursionExamples;
+import pl.smtd.recursion.RecursionTest1;
+import pl.smtd.recursion.RecursionTest2;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+    static Map<Integer, ConsoleExercise> EXAMPLES = new HashMap<>();
 
     public static void main(String[] args) {
-        RecursionExamples recursion = new RecursionExamples();
-        recursion.select();
+        addExamples(new RecursionExamples());
+
+        ConsoleMenu topMenu = new ConsoleMenu("Main menu", 35, EXAMPLES);
+        topMenu.show();
+    }
+
+    private static void addExamples(ConsoleExercise... exercises) {
+        for(int i = 0; i < exercises.length; i++) {
+            EXAMPLES.put(i, exercises[i]);
+        }
     }
 }

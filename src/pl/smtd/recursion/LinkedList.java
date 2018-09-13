cbb69e10f29e3f121913a138bf958a4d158e34a7
Piotr Ponikowski <piopon.github@gmail.com>
2018-09-13 19:40:53 +0200

    moved list to separate class + added second recursive method

--- src/pl/smtd/recursion/LinkedList.java
+++ src/pl/smtd/recursion/LinkedList.java
@@ -3,14 +3,15 @@ package pl.smtd.recursion;
 import pl.smtd.menu.ConsoleExample;
 
 public class LinkedList implements ConsoleExample {
+    TestList list = new TestList();
+
     @Override
     public void execute() {
-        Node nodeLast = new Node("3", null);
-        Node nodeMiddle = new Node("2", nodeLast);
-        Node nodeFirst = new Node("1", nodeMiddle);
-        System.out.println("Connection sequence:");
-        printLinkedList(nodeFirst);
+        fillList();
+        System.out.println("Connection sequence (" + list.getSize() + " elements): ");
+        list.print();
         System.out.println(" ");
+        list.clear();
     }
 
     @Override
@@ -18,22 +19,106 @@ public class LinkedList implements ConsoleExample {
         return "Linked list";
     }
 
-    private static void printLinkedList(Node n) {
-        if(n != null) {
-            System.out.print(n + " --> ");
-            printLinkedList(n.next);
-        } else {
-            System.out.print("[null]");
+    private void fillList() {
+        list.add(new TestNode("1"));
+        list.insert(new TestNode("2"),1);
+        list.add(new TestNode("3"));
+        list.add(new TestNode("5"));
+        list.insert(new TestNode("4"),3);
+        list.insert(new TestNode("last"),10);
+    }
+
+    private class TestList {
+        private TestNode headNode = null;
+
+        public void add(TestNode newNode) {
+            if (headNode == null) {
+                headNode = newNode;
+            } else {
+                TestNode lastNode = getLastNode();
+                lastNode.setNext(newNode);
+            }
+        }
+
+        public void insert(TestNode newNode, int position) {
+            if (position == 0) {
+                TestNode oldHeadNode = headNode;
+                headNode = newNode;
+                headNode.setNext(oldHeadNode);
+            } else if (position >= getSize()) {
+                add(newNode);
+            } else {
+                TestNode prevNode = getNode(position - 1);
+                TestNode currentNode = getNode(position);
+                prevNode.setNext(newNode);
+                newNode.setNext(currentNode);
+            }
+        }
+
+        public int getSize() {
+            int counter = 0;
+            TestNode currentNode = headNode;
+            while (currentNode != null) {
+                counter++;
+                currentNode = currentNode.getNext();
+            }
+            return counter;
+        }
+
+        public void clear() {
+            TestNode nextNode = headNode;
+            while (nextNode != null) {
+                TestNode currentNode = nextNode;
+                nextNode = currentNode.getNext();
+                currentNode.setNext(null);
+            }
+        }
+
+        public void print() {
+            print(headNode);
+        }
+
+        private TestNode getLastNode() {
+            return getNode(headNode, getSize() - 1);
+        }
+
+        private TestNode getNode(int pos) {
+            return getNode(headNode, pos);
+        }
+
+        private TestNode getNode(TestNode currentNode, int pos) {
+            if (currentNode.getNext() == null || pos == 0) {
+                return currentNode;
+            } else {
+                currentNode = getNode(currentNode.getNext(), --pos);
+            }
+            return currentNode;
+        }
+
+        private void print(TestNode n) {
+            if (n != null) {
+                System.out.print(n + " --> ");
+                print(n.next);
+            } else {
+                System.out.print("[null]");
+            }
         }
     }
 
-    private class Node {
-        String id;
-        Node next;
+    private class TestNode {
+        private String id;
+        private TestNode next;
 
-        public Node (String id, Node next) {
+        public TestNode(String id) {
             this.id = id;
-            this.next = next;
+        }
+
+        public TestNode getNext() {
+            return next;
+        }
+
+        public void setNext(TestNode nextNode) {
+            this.next = nextNode;
         }
 
         public String toString () {

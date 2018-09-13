a8ea14d1ead78e30e3b398cb9f2c2ab5d2017b25
Piotr Ponikowski <piopon.github@gmail.com>
2018-09-13 19:48:50 +0200

    moved TestList and TestNode classes to separare files

---
+++ src/pl/smtd/recursion/list/TestNode.java
@@ -0,0 +1,22 @@
+package pl.smtd.recursion.list;
+
+public class TestNode {
+    private String id;
+    private TestNode next;
+
+    public TestNode(String id) {
+        this.id = id;
+    }
+
+    public TestNode getNext() {
+        return next;
+    }
+
+    public void setNext(TestNode nextNode) {
+        this.next = nextNode;
+    }
+
+    public String toString () {
+        return "Node id: " + id;
+    }
+}

1dbdbf4d8b126686a9229c5cfdb0134ad82df53a
Piotr Ponikowski <piopon.github@gmail.com>
2018-09-14 19:40:54 +0200

    added non recursion factorial example to compare times

--- src/pl/smtd/recursion/factorial/Factorial.java
+++ src/pl/smtd/recursion/factorial/Factorial.java
@@ -3,6 +3,7 @@ package pl.smtd.recursion.factorial;
 import pl.smtd.menu.ConsoleExample;
 
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class Factorial implements ConsoleExample {
     @Override
@@ -10,7 +11,8 @@ public class Factorial implements ConsoleExample {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the number to calculate the factorial: ");
         int input = in.nextInt();
-        System.out.print("RESULT: " + input + "! = "  + factorial(input));
+        System.out.println("RESULT (recursive): " + comparisonOutput(this::factorial, input));
+        System.out.println("RESULT (in a loop): " + comparisonOutput(this::factorialLoop, input));
         System.out.println(" ");
     }
 
@@ -24,5 +26,21 @@ public class Factorial implements ConsoleExample {
             return 1;
         }
         return num * factorial(num - 1);
+    }
+
+    public int factorialLoop(int num) {
+        int result = 1;
+        for (int i = 2; i <= num; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    private String comparisonOutput(Function<Integer, Integer> function, int number) {
+        long timeStart = System.nanoTime();
+        int result = function.apply(number);
+        long timeStop = System.nanoTime();
+
+        return number + "! = "  + result + " [ time: " + (timeStop - timeStart) + "ns ]";
     }
 }

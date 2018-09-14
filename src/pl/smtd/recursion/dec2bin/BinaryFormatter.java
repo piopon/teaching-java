5085957908b405581773b49d8a9e770c98e59a7b
Piotr Ponikowski <piopon.github@gmail.com>
2018-09-14 17:51:54 +0200

    moved formatting binary output to separate class

---
+++ src/pl/smtd/recursion/dec2bin/BinaryFormatter.java
@@ -0,0 +1,26 @@
+package pl.smtd.recursion.dec2bin;
+
+public class BinaryFormatter {
+
+    public static String formatBin(String bin, int bitsPerGroup ) {
+        return divideGroup(fillToGroup(bin, bitsPerGroup), bitsPerGroup);
+    }
+
+    private static String fillToGroup(String input, int groupSize) {
+        String output = input;
+        int groups = (input.length() / groupSize) + (input.length() % groupSize);
+        for (int i = 0; i < groups * groupSize - input.length(); i++) {
+            output = "0" + output;
+        }
+        return output;
+    }
+
+    private static String divideGroup(String input, int groupSize) {
+        StringBuilder out = new StringBuilder(input);
+        int divideCount = input.length() / groupSize - 1;
+        for (int i = divideCount; i > 0; i--) {
+            out.insert(i * groupSize, " ");
+        }
+        return out.toString();
+    }
+}
--- src/pl/smtd/recursion/dec2bin/DecimalToBinary.java
+++ src/pl/smtd/recursion/dec2bin/DecimalToBinary.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class DecimalToBinary implements ConsoleExample {
     int minInt = 1;
-    int maxInt = 1000;
+    int maxInt = 65535;
     private String prompt = "Enter an integer [" + minInt + " - " + maxInt + "]: ";
 
     @Override
@@ -16,7 +16,7 @@ public class DecimalToBinary implements ConsoleExample {
         int decimal = input.nextInt();
         if (decimal >= minInt && decimal <= maxInt) {
             System.out.print("DECIMAL: " + decimal + " -> BINARY: ");
-            System.out.print(decToBin(decimal));
+            System.out.print(BinaryFormatter.formatBin(decToBin(decimal), 4));
             System.out.println(" ");
         } else {
             System.out.println("ERROR: Wrong number");
@@ -28,35 +28,10 @@ public class DecimalToBinary implements ConsoleExample {
         return "Decimal to Binary";
     }
 
-    private static String decToBin(int dec) {
+    public static String decToBin(int dec) {
         if (dec > 0) {
             return decToBin(dec / 2) + dec%2;
         }
         return "";
-    }
-
-    private static String formatBin(String bin) {
-        int bitsPerGroup = 4;
-
-        return divideGroup(fillToGroup(bin, bitsPerGroup), bitsPerGroup);
-    }
-
-    private static String fillToGroup(String input, int groupSize) {
-        String output = input;
-        int groups = (input.length()/groupSize + 1);
-        int zerosToAdd = groups * groupSize - input.length();
-        for (int i = 0; i < zerosToAdd; i++) {
-            output = "0" + output;
-        }
-        return output;
-    }
-
-    private static String divideGroup(String input, int groupSize) {
-        StringBuilder out = new StringBuilder(input);
-        int divideCount = input.length() / groupSize - 1;
-        for (int i = divideCount; i > 0; i--) {
-            out.insert(i * groupSize, " ");
-        }
-        return out.toString();
     }
 }

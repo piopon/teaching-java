4805826e07cf259abbb0bc988534fb64a9c9cda2
Piotr Ponikowski <piopon.github@gmail.com>
2018-09-15 16:37:22 +0200

    binary formatter unit tests update: 3 bits per group

--- test/pl/smtc/recursion/dec2bin/BinaryFormatterTest.java
+++ test/pl/smtc/recursion/dec2bin/BinaryFormatterTest.java
@@ -33,4 +33,26 @@ class BinaryFormatterTest {
                 Arguments.of("101101110100000", "0101 1011 1010 0000"),
                 Arguments.of("1111111111111111", "1111 1111 1111 1111"));
     }
+
+    @ParameterizedTest(name = "{0} after format = {1}")
+    @MethodSource("create3BitsPerGroupData")
+    void formatBinShouldCorrectlyFormatBinaryIn3BitsPerGroup(String binRaw, String binFormat) {
+        assertEquals(binFormat, BinaryFormatter.formatBin(binRaw, 3));
+    }
+
+    private static Stream<Arguments> create3BitsPerGroupData() {
+        return Stream.of(
+                Arguments.of("1", "001"),
+                Arguments.of("10", "010"),
+                Arguments.of("100", "100"),
+                Arguments.of("1111", "001 111"),
+                Arguments.of("11111", "011 111"),
+                Arguments.of("11111110", "011 111 110"),
+                Arguments.of("11111111", "011 111 111"),
+                Arguments.of("100000000", "100 000 000"),
+                Arguments.of("100000000000", "100 000 000 000"),
+                Arguments.of("10101101100111", "010 101 101 100 111"),
+                Arguments.of("101101110100000", "101 101 110 100 000"),
+                Arguments.of("1111111111111111", "001 111 111 111 111 111"));
+    }
 }

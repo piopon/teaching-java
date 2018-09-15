06f65d537ce369d3a7bf0f54f7b68047b8f98d89
Piotr Ponikowski <piopon.github@gmail.com>
2018-09-15 15:44:32 +0200

    added JUnit5 parameterized tests + updated dec to bin unit tests

--- test/pl/smtc/recursion/dec2bin/DecimalToBinaryTest.java
+++ test/pl/smtc/recursion/dec2bin/DecimalToBinaryTest.java
@@ -2,6 +2,11 @@ package pl.smtc.recursion.dec2bin;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +22,30 @@ class DecimalToBinaryTest {
     void getNameShouldReturnDecimalToBinary() {
         String currentName = decToBin.getName();
         assertEquals("Decimal to Binary", currentName);
+    }
+
+    @Test
+    void decToBinShouldThrowOnNegativeNumbers() {
+        String errorMessage = "ERROR: only positive numbers!";
+        assertThrows(IllegalArgumentException.class, () -> DecimalToBinary.decToBin(-1), errorMessage);
+    }
+
+    @ParameterizedTest(name = "{0} [DEC] = {1} [BIN]")
+    @MethodSource("createDecToBinData")
+    void decToBinShouldCorrectlyConvertDecimalNumber(int decNumber, String binNumber) {
+        assertEquals(binNumber, DecimalToBinary.decToBin(decNumber));
+    }
+
+    private static Stream<Arguments> createDecToBinData() {
+        return Stream.of(
+                Arguments.of(1, "1"),
+                Arguments.of(4, "100"),
+                Arguments.of(15, "1111"),
+                Arguments.of(254, "11111110"),
+                Arguments.of(255, "11111111"),
+                Arguments.of(256, "100000000"),
+                Arguments.of(11111, "10101101100111"),
+                Arguments.of(23456, "101101110100000"),
+                Arguments.of(65535, "1111111111111111"));
     }
 }

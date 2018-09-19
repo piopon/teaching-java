11544c0ee495cedbb3f90263a86851534780cee1
Piotr Ponikowski <piopon.github@gmail.com>
2018-09-19 22:58:35 +0200

    main menu application simple unit test

---
+++ test/pl/smtc/MainTest.java
@@ -0,0 +1,40 @@
+package pl.smtc;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+    private Main mainApp;
+    private static final ByteArrayOutputStream OUTPUT_STREAM = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setup() {
+        mainApp = new Main();
+        System.setOut(new PrintStream(OUTPUT_STREAM));
+    }
+
+    @AfterEach
+    void teardown() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    void main() {
+        simulateUserInput("0");
+        Main.main(new String[]{""});
+        String output = OUTPUT_STREAM.toString();
+        assertTrue(output.contains("MAIN MENU"));
+        assertTrue(output.contains("1) Recursion examples"));
+    }
+
+    private void simulateUserInput(String simInput) {
+        System.setIn(new ByteArrayInputStream(simInput.getBytes()));
+    }
+}

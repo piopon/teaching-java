b5efe1c70c55931dcd43facfc708f5a8e6efb0f2
Piotr Ponikowski <piopon.github@gmail.com>
2018-09-29 12:31:04 +0200

    added synchronizing threads example: sync code block

---
+++ src/pl/smtc/threads/sync/block/Countdown.java
@@ -0,0 +1,20 @@
+package pl.smtc.threads.sync.block;
+
+public class Countdown {
+    private int decr;
+    private int startValue;
+
+    public Countdown(int startValue) {
+        this.startValue = startValue;
+    }
+
+    public void countdown(String countdownTitle) {
+        System.out.println(countdownTitle + " -> START");
+        synchronized (this) {
+            for (decr = startValue; decr > 0; decr--) {
+                System.out.println(countdownTitle + " -> countdown: " + decr);
+            }
+        }
+        System.out.println(countdownTitle + " -> STOP");
+    }
+}

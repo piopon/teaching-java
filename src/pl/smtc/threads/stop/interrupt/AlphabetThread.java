8cb1a3c608071df26f9d57c702bcc3a259fef635
Piotr Ponikowski <piopon.github@gmail.com>
2018-10-08 23:42:04 +0200

    updated alphabet thread class to be more user friendly

--- src/pl/smtc/threads/stop/interrupt/AlphabetThread.java
+++ src/pl/smtc/threads/stop/interrupt/AlphabetThread.java
@@ -1,13 +1,15 @@
 package pl.smtc.threads.stop.interrupt;
 
+import java.util.concurrent.TimeUnit;
+
 public class AlphabetThread extends Thread {
     private final static char START_CHAR = 'A';
     private final static char STOP_CHAR = 'Z';
 
     private int id;
-    private boolean printout;
-    private long quickness;
-    private char alpha;
+    private boolean printout = false;
+    private long quickness = 50;
+    private long timeout = TimeUnit.MINUTES.toNanos(5);
 
     public AlphabetThread(int id, boolean printout, long quickness) {
         this.id = id;
@@ -15,16 +17,26 @@ public class AlphabetThread extends Thread {
         this.quickness = quickness;
     }
 
+    public AlphabetThread(int id, long timeoutMs) {
+        this.id = id;
+        this.timeout = TimeUnit.MILLISECONDS.toNanos(timeoutMs);
+    }
+
     @Override
     public void run() {
         System.out.print("AlphabetThread " + id + " -> run [start]");
+        char alpha = START_CHAR;
         try {
+            long startTime = System.nanoTime();
             for (alpha = START_CHAR; alpha <= STOP_CHAR; alpha++) {
                 if (printout) {
                     System.out.print(alpha + " ");
                 }
                 Thread.sleep(quickness);
                 alpha = (alpha == STOP_CHAR) ? START_CHAR : alpha;
+                if ((System.nanoTime() - startTime) > timeout) {
+                    Thread.currentThread().interrupt();
+                }
             }
         } catch (InterruptedException e) {
             System.out.println("AlphabetThread " + id + " -> interrupted!");

b56ec3ece9cf56cf0210ef5b21753c7a22bee79b
Piotr Ponikowski <piopon.github@gmail.com>
2018-12-06 17:40:08 +0100

    another fix try of nested lockout and race condition unit tests

--- test/pl/smtc/threads/issues/race/RaceConditionTest.java
+++ test/pl/smtc/threads/issues/race/RaceConditionTest.java
@@ -3,7 +3,9 @@ package pl.smtc.threads.issues.race;
 import org.junit.jupiter.api.Test;
 import pl.smtc.base.BaseTestInOut;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,14 +20,21 @@ class RaceConditionTest extends BaseTestInOut {
 
     @Test
     void executeShouldInvokeRaceConditionExample() {
-        simulateUserInput("5");
+        simulateUserInput("10");
         raceCondition.execute();
         String output = getOutput();
-        String notExpectedSequence[] = {"Thread 1 -> START", "Thread 1 -> counter: 1", "Thread 1 -> counter: 2",
-                "Thread 1 -> counter: 3", "Thread 1 -> counter: 4", "Thread 1 -> counter: 5", "Thread 1 -> STOP",
-                "Thread 2 -> START", "Thread 2 -> counter: 1", "Thread 2 -> counter: 2", "Thread 2 -> counter: 3",
-                "Thread 2 -> counter: 4", "Thread 2 -> counter: 5", "Thread 2 -> STOP"};
-        assertFalse(outputContainsInOrder(output, notExpectedSequence));
+        String notExpectedSequenceThread1[] = { "Thread 1 -> START", "Thread 1 -> counter: 1",
+                "Thread 1 -> counter: 2", "Thread 1 -> counter: 3", "Thread 1 -> counter: 4",
+                "Thread 1 -> counter: 5", "Thread 1 -> counter: 6", "Thread 1 -> counter: 7",
+                "Thread 1 -> counter: 8","Thread 1 -> counter: 9","Thread 1 -> counter: 10",
+                "Thread 1 -> STOP"};
+        assertFalse(outputContainsInOrder(output, notExpectedSequenceThread1));
+        String notExpectedSequenceThread2[] = { "Thread 2 -> START", "Thread 2 -> counter: 1",
+                "Thread 2 -> counter: 2", "Thread 2 -> counter: 3", "Thread 2 -> counter: 4",
+                "Thread 2 -> counter: 5", "Thread 2 -> counter: 6", "Thread 2 -> counter: 7",
+                "Thread 2 -> counter: 8", "Thread 2 -> counter: 9", "Thread 2 -> counter: 10",
+                "Thread 2 -> STOP"};
+        assertFalse(outputContainsInOrder(output, notExpectedSequenceThread2));
     }
 
     @Test

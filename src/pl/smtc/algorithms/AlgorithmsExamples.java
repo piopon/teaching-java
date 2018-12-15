7afa0fcfd2992954df5a8109332fb8a0c308ae10
Piotr Ponikowski <piopon.github@gmail.com>
2018-12-15 12:51:25 +0100

    added new top level menu: algorithms + simple unit tests

---
+++ src/pl/smtc/algorithms/AlgorithmsExamples.java
@@ -0,0 +1,33 @@
+package pl.smtc.algorithms;
+
+import pl.smtc.algorithms.search.SearchAlgorithms;
+import pl.smtc.algorithms.sort.SortAlgorithms;
+import pl.smtc.menu.ConsoleExample;
+import pl.smtc.menu.ConsoleMenu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AlgorithmsExamples implements ConsoleExample {
+    private Map<Integer, ConsoleExample> algorithmExamples = new HashMap<>();
+    private int menuWidth = 35;
+
+    @Override
+    public void execute() {
+        addExamples(new SortAlgorithms(), new SearchAlgorithms());
+
+        ConsoleMenu algorithmsMenu = new ConsoleMenu(getName(), menuWidth, algorithmExamples);
+        algorithmsMenu.show();
+    }
+
+    @Override
+    public String getName() {
+        return "Algorithm examples";
+    }
+
+    private void addExamples(ConsoleExample... examples) {
+        for(int i = 0; i < examples.length; i++) {
+            algorithmExamples.put(i, examples[i]);
+        }
+    }
+}

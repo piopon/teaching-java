a6add415d025e84cdd72f573862fb1e12f5ee94d
Piotr Ponikowski <piopon.github@gmail.com>
2018-12-15 13:40:06 +0100

    added bubble sort unit tests

---
+++ test/pl/smtc/algorithms/sort/bubble/BubbleSortTest.java
@@ -0,0 +1,53 @@
+package pl.smtc.algorithms.sort.bubble;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import pl.smtc.base.BaseTestOut;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest extends BaseTestOut {
+    private BubbleSort bubbleSort;
+
+    @Override
+    protected void setUp() {
+        bubbleSort = new BubbleSort();
+    }
+
+    @Test
+    void executeShouldInvokeBubbleSortExample() {
+        bubbleSort.execute();
+        String output = getOutput();
+        assertTrue(output.contains("TO SORT: [10, -9, 1, 0, -1, 4, 3, -3, 2, 1]"));
+        assertTrue(output.contains("SORTED: [-9, -3, -1, 0, 1, 1, 2, 3, 4, 10]"));
+    }
+
+    @Test
+    void getNameShouldReturnBubbleSortString() {
+        assertEquals("Bubble sort", bubbleSort.getName());
+    }
+
+    @ParameterizedTest(name = "Bubble sorted array: {1}")
+    @MethodSource("sortTestData")
+    void sortShouldCorrectlySortInputTable(List<Integer> toSort, List<Integer> sorted) {
+        assertEquals(sorted, bubbleSort.sort(toSort));
+    }
+
+    private static Stream<Arguments> sortTestData() {
+        return Stream.of(
+            Arguments.of(Arrays.asList(1, 1, 1, 1, 1, 1), Arrays.asList(1, 1, 1, 1, 1, 1)),
+            Arguments.of(Arrays.asList(1, 2, -1, -2, 0), Arrays.asList(-2, -1, 0, 1, 2)),
+            Arguments.of(Arrays.asList(0, 1, 2, 3, 4), Arrays.asList(0, 1, 2, 3, 4)),
+            Arguments.of(Arrays.asList(-5, -4, -3, -2, -1), Arrays.asList(-5, -4, -3, -2, -1)),
+            Arguments.of(Arrays.asList(-100, 100, 1, -5, -1), Arrays.asList(-100, -5, -1, 1, 100)),
+            Arguments.of(Arrays.asList(-3, -3, 7, 7, 0, 0), Arrays.asList(-3, -3, 0, 0, 7, 7)),
+            Arguments.of(Arrays.asList(-1, -1, -1, -1, -1, -1), Arrays.asList(-1, -1, -1, -1, -1, -1)),
+            Arguments.of(Arrays.asList(0, 0, 0, -1, 1), Arrays.asList(-1, 0, 0, 0, 1)));
+    }
+}

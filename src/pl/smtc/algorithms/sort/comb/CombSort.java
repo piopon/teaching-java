4921902dc5486004705f0861ea819ce9d212bfdf
Piotr Ponikowski <piopon.github@gmail.com>
2018-12-30 19:31:30 +0100

    new comb sort algorithm (+ unit tests)

---
+++ src/pl/smtc/algorithms/sort/comb/CombSort.java
@@ -0,0 +1,54 @@
+package pl.smtc.algorithms.sort.comb;
+
+import pl.smtc.menu.ConsoleExample;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombSort implements ConsoleExample {
+    private static double SHRINK_FACTOR = 1.3;
+
+    @Override
+    public void execute() {
+        List<Integer> toSort = Arrays.asList(10, -9, 1, 0, -1, 4, 3, -3, 1, 2);
+        System.out.println("TO SORT: " + toSort);
+        System.out.println("SORTED:  " + sort(toSort));
+    }
+
+    @Override
+    public String getName() {
+        return "Comb sort";
+    }
+
+    public List<Integer> sort(final List<Integer> input) {
+        List<Integer> result = new ArrayList<>(input);
+        int gap = input.size();
+        boolean swapped = true;
+        while (gap != 1 || swapped) {
+            swapped = false;
+            gap = shrinkGap(gap);
+            for (int i=0; i < result.size() - gap; i++) {
+                if (result.get(i) > result.get(i+gap)) {
+                    swap(result, i, i+gap);
+                    swapped = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    private void swap(final List<Integer> list, int firstIndex, int secondIndex) {
+        int temp = list.get(firstIndex);
+        list.set(firstIndex, list.get(secondIndex));
+        list.set(secondIndex, temp);
+    }
+
+    private int shrinkGap(int gap) {
+        gap /= SHRINK_FACTOR;
+        if (gap < 1) {
+            gap = 1;
+        }
+        return gap;
+    }
+}

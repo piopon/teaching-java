e7bb6596eaec0e8700e8897ff818acc1c81db1e2
Piotr Ponikowski <piopon.github@gmail.com>
2019-01-01 22:46:24 +0100

    added radix sort and unit tests

--- src/pl/smtc/algorithms/sort/radix/RadixSort.java
+++ src/pl/smtc/algorithms/sort/radix/RadixSort.java
@@ -7,13 +7,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RadixSort implements ConsoleExample {
-    private static final int CUTOFF = 15;
-    private static final int BITS_PER_INT = 32;
-    private static final int BITS_PER_BYTE = 8;
-    private static final int R = 256;
+    private static final int BYTE_MASK = 0b1111_1111;
+    private static final int BYTE_MAX = 256;
 
     @Override
     public void execute() {
+
         List<Integer> toSort = Arrays.asList(10, -9, 1, 0, -1, 4, 3, -3, 1, 2);
         System.out.println("TO SORT: " + toSort);
         System.out.println("SORTED:  " + sort(toSort));
@@ -27,93 +26,78 @@ public class RadixSort implements ConsoleExample {
     public List<Integer> sort(final List<Integer> input) {
         List<Integer> result = new ArrayList<>(input);
 
-        int firstPositiveIndex = partition(0, result);
-        int[] aux = new int[result.size()];
-        if (firstPositiveIndex > 0) {
-            recSort(result, firstPositiveIndex, result.size() - 1, 0, aux);
-            recSort(result, 0, firstPositiveIndex - 1, 0, aux);
+        int positiveIndex = rearrangeList(result);
+        int[] helpArray = new int[result.size()];
+        if (positiveIndex > 0) {
+            sort(result, positiveIndex, result.size()-1, 0, helpArray);
+            sort(result, 0, positiveIndex-1, 0, helpArray);
         } else {
-            recSort(result, 0, result.size() - 1, 0, aux);
+            sort(result, 0, result.size()-1, 0, helpArray);
         }
         return result;
     }
 
-    private void recSort(final List<Integer> a, int lo, int hi, int d, int[] aux) {
-        if (d > 4) return;
-        if (hi - lo < CUTOFF) {
-            insertionSort(a, lo, hi);
+    private void sort(final List<Integer> list, int start, int end, int startByte, int[] help) {
+        if (startByte > Integer.SIZE / Byte.SIZE) {
             return;
         }
 
-        int[] count = new int[R + 1];
-
-        int bitsToShift = BITS_PER_INT - BITS_PER_BYTE * d - BITS_PER_BYTE;
-        int mask = 0b1111_1111;
-        for (int i = lo; i <= hi; i++) {
-            int c = (a.get(i) >> bitsToShift) & mask;
-            count[c + 1]++;
+        int[] count = new int[BYTE_MAX + 1];
+        int bitsToShift = Integer.SIZE - Byte.SIZE * (startByte - 1);
+        for (int i = start; i <= end; i++) {
+            int c = (list.get(i) >> bitsToShift) & BYTE_MASK;
+            count[c+1]++;
         }
 
-        for (int i = 0; i < R; i++) {
-            count[i + 1] = count[i] + count[i + 1];
+        for (int i = 0; i < BYTE_MAX; i++) {
+            count[i+1] = count[i] + count[i + 1];
         }
 
-        for (int i = lo; i <= hi; i++) {
-            int c = (a.get(i) >> bitsToShift) & mask;
-            aux[count[c] + lo] = a.get(i);
+        for (int i = start; i <= end; i++) {
+            int c = (list.get(i) >> bitsToShift) & BYTE_MASK;
+            help[count[c] + start] = list.get(i);
             count[c]++;
         }
 
-        for (int i = lo; i <= hi; i++) {
-            a.set(i, aux[i]);
+        for (int i = start; i <= end; i++) {
+            list.set(i, help[i]);
         }
 
         if (count[0] > 0) {
-            recSort(a, lo, lo + count[0] - 1, d + 1, aux);
+            sort(list, start, start+count[0]-1, startByte+1, help);
         }
 
-        for (int i = 1; i < R; i++) {
+        for (int i = 1; i < BYTE_MAX; i++) {
             if (count[i] > 0) {
-                recSort(a, lo + count[i - 1], lo + count[i] - 1, d + 1, aux);
+                sort(list, start+count[i-1], start+count[i]-1, startByte+1, help);
             }
         }
     }
 
-    private void insertionSort(final List<Integer> a, int lo, int hi) {
-        for (int i = lo; i <= hi; i++) {
-            for (int j = i; j > lo && a.get(j) < a.get(j - 1); j--) {
-                swap(a, j, j - 1);
-            }
-        }
-    }
+    private int rearrangeList(final List<Integer> list) {
+        int currentStart = 0;
+        int currentStop = list.size()-1;
 
-    private int partition(int pivot, List<Integer> a) {
-        int curLo = 0;
-        int curHi = a.size()-1;
-
-        while (curLo < curHi) {
-
-            while (a.get(curLo) < pivot) {
-                if ((curLo + 1) > a.size()-1) {
-                    return a.size();
+        while (currentStart <= currentStop) {
+            while (list.get(currentStart) < 0) {
+                if ((currentStart + 1) > list.size()-1) {
+                    return list.size();
                 }
-                curLo++;
+                currentStart++;
             }
-
-            while (a.get(curHi) >= pivot) {
-                if ((curHi - 1) < 0) {
+            while (list.get(currentStop) >= 0) {
+                if ((currentStop - 1) < 0) {
                     return - 1;
                 }
-                curHi--;
+                currentStop--;
             }
-
-            if (curLo < curHi) {
-                swap(a, curLo, curHi);
-                if (a.get(curLo) != pivot) curLo++;
-                if (a.get(curHi) != pivot) curHi--;
+            if (currentStart < currentStop) {
+                swap(list, currentStart, currentStop);
+                if (list.get(currentStart) != 0) currentStart++;
+                if (list.get(currentStop) != 0) currentStop--;
             }
         }
-        return curLo;
+        return currentStart;
     }
 
     private void swap(final List<Integer> list, int firstIndex, int secondIndex) {

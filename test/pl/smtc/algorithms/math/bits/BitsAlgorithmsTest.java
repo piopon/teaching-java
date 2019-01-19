8a3f9a2bb545ee4888978265171734bce622bb71
Piotr Ponikowski <piopon.github@gmail.com>
2019-01-19 17:33:45 +0100

    added tests for bits algorithm menu

---
+++ test/pl/smtc/algorithms/math/bits/BitsAlgorithmsTest.java
@@ -0,0 +1,29 @@
+package pl.smtc.algorithms.math.bits;
+
+import org.junit.jupiter.api.Test;
+import pl.smtc.base.BaseTestInOut;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BitsAlgorithmsTest extends BaseTestInOut {
+    private BitsAlgorithms bitsAlgorithms;
+
+    @Override
+    protected void setUp() {
+        bitsAlgorithms = new BitsAlgorithms();
+    }
+
+    @Test
+    void executeShouldInvokeBitsAlgorithmsMenu() {
+        simulateUserInput("0" + System.lineSeparator());
+        bitsAlgorithms.execute();
+        String output = getOutput();
+    }
+
+    @Test
+    void getNameShouldReturnBitsAlgorithmsString() {
+        assertEquals("Bits algorithms", bitsAlgorithms.getName());
+    }
+
+
+}

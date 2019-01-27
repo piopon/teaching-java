c39c2c820ef4e42cfbabfe3bda40950be8c83ffe
Piotr Ponikowski <piopon.github@gmail.com>
2019-01-27 22:32:28 +0100

    new bits switcher example

--- src/pl/smtc/algorithms/math/bits/BitsAlgorithms.java
+++ src/pl/smtc/algorithms/math/bits/BitsAlgorithms.java
@@ -4,6 +4,7 @@ import pl.smtc.algorithms.math.bits.gray.GrayCode;
 import pl.smtc.algorithms.math.bits.increment.BitIncrementer;
 import pl.smtc.algorithms.math.bits.opposite.OppositeSigns;
 import pl.smtc.algorithms.math.bits.converter.FormatConverter;
+import pl.smtc.algorithms.math.bits.switcher.BitsSwitcher;
 import pl.smtc.menu.ConsoleExample;
 import pl.smtc.menu.ConsoleMenu;
 
@@ -17,7 +18,7 @@ public class BitsAlgorithms implements ConsoleExample {
     @Override
     public void execute() {
         addExamples(new BitIncrementer(), new OppositeSigns(), new GrayCode(),
-                    new FormatConverter());
+                    new FormatConverter(), new BitsSwitcher());
 
         ConsoleMenu bitsAlgorithmsMenu = new ConsoleMenu(getName(), menuWidth, bitsAlgorithms);
         bitsAlgorithmsMenu.show();

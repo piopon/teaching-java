d8df585c150128dfd0336f803c05b3f5402623d0
Piotr Ponikowski <piopon.github@gmail.com>
2019-02-04 23:16:42 +0100

    command pattern: added invoker class

---
+++ src/pl/smtc/patterns/behavioral/command/invokers/CommandManager.java
@@ -0,0 +1,35 @@
+package pl.smtc.patterns.behavioral.command.invokers;
+
+import pl.smtc.patterns.behavioral.command.actions.ICommand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandManager {
+    private List<ICommand> history = new ArrayList<>();
+    private int lastCommand = -1;
+
+    public void exe(ICommand command) {
+        lastCommand++;
+        history.add(command);
+        if (lastCommand != history.size()-1) {
+            history.remove(lastCommand);
+        }
+        command.execute();
+    }
+
+    public void undo() {
+        if (lastCommand > 0) {
+            history.get(lastCommand).unexecute();
+            lastCommand--;
+        }
+    }
+
+    public void redo() {
+        if (lastCommand < history.size()-1) {
+            lastCommand++;
+            history.get(lastCommand).execute();
+        }
+    }
+
+}

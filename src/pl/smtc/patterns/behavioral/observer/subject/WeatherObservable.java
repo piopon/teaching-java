ac699db3c2529787c23239b6580ed349a4112176
Piotr Ponikowski <piopon.github@gmail.com>
2019-02-16 22:04:47 +0100

    observer pattern: added observable abstract class

---
+++ src/pl/smtc/patterns/behavioral/observer/subject/WeatherObservable.java
@@ -0,0 +1,24 @@
+package pl.smtc.patterns.behavioral.observer.subject;
+
+import pl.smtc.patterns.behavioral.observer.subscriber.WeatherObserver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class WeatherObservable {
+    private List<WeatherObserver> observers = new ArrayList<>();
+
+    public void add(WeatherObserver observer) {
+        observers.add(observer);
+        System.out.println("Added observer: " + observer.getId() + " [subscribers: " + observers.size() + "]");
+    }
+
+    public void remove(WeatherObserver observer) {
+        observers.remove(observer);
+        System.out.println("Removed observer: " + observer.getId() + " [subscribers: " + observers.size() + "]");
+    }
+
+    public void notifyChanged() {
+        observers.forEach(WeatherObserver::update);
+    }
+}

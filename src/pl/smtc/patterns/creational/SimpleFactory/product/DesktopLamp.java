9668a7f3c212d6a8c77a64a23defa1546f54c09e
Piotr Ponikowski <piopon.github@gmail.com>
2019-02-27 22:08:15 +0100

    added factory product (lamp) implementations

---
+++ src/pl/smtc/patterns/creational/SimpleFactory/product/DesktopLamp.java
@@ -0,0 +1,25 @@
+package pl.smtc.patterns.creational.SimpleFactory.product;
+
+import pl.smtc.patterns.creational.SimpleFactory.factory.Lamps;
+
+public class DesktopLamp implements ILamp {
+    private Lamps type;
+    private String mounting;
+    private String supply;
+
+    public DesktopLamp(String mounting, String supply) {
+        this.type = Lamps.DESKTOP;
+        this.mounting = mounting;
+        this.supply = supply;
+    }
+
+    @Override
+    public String getDescription() {
+        return type + " LAMP [mount: " + mounting + ", power supply: " + supply + "]";
+    }
+
+    @Override
+    public Lamps getType() {
+        return type;
+    }
+}
---
+++ src/pl/smtc/patterns/creational/SimpleFactory/product/WallLamp.java
@@ -0,0 +1,25 @@
+package pl.smtc.patterns.creational.SimpleFactory.product;
+
+import pl.smtc.patterns.creational.SimpleFactory.factory.Lamps;
+
+public class WallLamp implements ILamp {
+    private Lamps type;
+    private String mounting;
+    private String supply;
+
+    public WallLamp(String mounting, String supply) {
+        this.type = Lamps.WALL;
+        this.mounting = mounting;
+        this.supply = supply;
+    }
+
+    @Override
+    public String getDescription() {
+        return type + " LAMP [mount: " + mounting + ", power supply: " + supply + "]";
+    }
+
+    @Override
+    public Lamps getType() {
+        return type;
+    }
+}

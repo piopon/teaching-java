2fbd9fa48ce828abfe72deeda7b2f0e72e512ced
Piotr Ponikowski <piopon.github@gmail.com>
2019-03-10 16:38:33 +0100

    added enums representing factory products types (lamp, mount, source)

---
+++ src/pl/smtc/patterns/creational/AbstractFactory/product/lamp/ELamp.java
@@ -0,0 +1,5 @@
+package pl.smtc.patterns.creational.AbstractFactory.product.lamp;
+
+public enum ELamp {
+    CEILING, DESKTOP, WALL, STREET, GARDEN, UNKNOWN
+}
---
+++ src/pl/smtc/patterns/creational/AbstractFactory/product/source/ESource.java
@@ -0,0 +1,5 @@
+package pl.smtc.patterns.creational.AbstractFactory.product.source;
+
+public enum ESource {
+    FLUORESCENT, NEON, HMI, CDM, LED, UNKNOWN
+}

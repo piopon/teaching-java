98d53f06915706ee66383219590f8b5f568bf7c2
Piotr Ponikowski <piopon.github@gmail.com>
2019-03-14 21:39:48 +0100

    implemented product abstract lamp classes

---
+++ src/pl/smtc/patterns/creational/AbstractFactory/product/lamp/GardenLamp.java
@@ -0,0 +1,20 @@
+package pl.smtc.patterns.creational.AbstractFactory.product.lamp;
+
+public class GardenLamp extends Lamp {
+
+    public GardenLamp(String caseColor) {
+        super(caseColor);
+        type = ELamp.GARDEN;
+        mountPrerequisites = "place to dig a hole for lamp";
+    }
+
+    @Override
+    public boolean power(boolean on) {
+        if (super.power(on)) {
+            System.out.println("You should have nice and mood light");
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/pl/smtc/patterns/creational/AbstractFactory/product/lamp/StreetLamp.java
@@ -0,0 +1,20 @@
+package pl.smtc.patterns.creational.AbstractFactory.product.lamp;
+
+public class StreetLamp extends Lamp {
+
+    public StreetLamp(String caseColor) {
+        super(caseColor);
+        type = ELamp.STREET;
+        mountPrerequisites = "city power connection, mount team";
+    }
+
+    @Override
+    public boolean power(boolean on) {
+        if (super.power(on)) {
+            System.out.println("Light cannot blind pedestrians, cyclists and drivers.");
+            return true;
+        }
+        return false;
+    }
+
+}

2d17f1631799022e17b3f334a280ef9fee6d0470
Piotr Ponikowski <piopon.github@gmail.com>
2019-03-17 01:45:46 +0100

    removed ununsed list and import from stand mount class

--- src/pl/smtc/patterns/creational/AbstractFactory/product/mount/StandMounting.java
+++ src/pl/smtc/patterns/creational/AbstractFactory/product/mount/StandMounting.java
@@ -1,9 +1,6 @@
 package pl.smtc.patterns.creational.AbstractFactory.product.mount;
 
-import java.util.List;
-
 public class StandMounting extends Mounting {
-    private List<String> tools;
 
     public StandMounting() {
         type = EMounting.STAND;

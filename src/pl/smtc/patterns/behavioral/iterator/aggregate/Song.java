77bb7603d804849c148e1521f07c50d33b80cd05
Piotr Ponikowski <piopon.github@gmail.com>
2019-05-16 17:22:28 +0200

    new iterator pattern example: added song aggregate class

---
+++ src/pl/smtc/patterns/behavioral/iterator/aggregate/Song.java
@@ -0,0 +1,19 @@
+package pl.smtc.patterns.behavioral.iterator.aggregate;
+
+public class Song {
+    private String artist;
+    private String title;
+
+    public Song(String artist, String title) {
+        this.artist = artist;
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}

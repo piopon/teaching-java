8063152e6bb6bff100dad10e5f73bb601ab608de
Piotr Ponikowski <piopon.github@gmail.com>
2019-06-02 13:20:38 +0200

    added unit tests for state support class: print queue

---
+++ test/pl/smtc/patterns/behavioral/state/support/PrintQueueTest.java
@@ -0,0 +1,63 @@
+package pl.smtc.patterns.behavioral.state.support;
+
+import org.junit.jupiter.api.Test;
+import pl.smtc.base.BaseTestOut;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrintQueueTest extends BaseTestOut {
+    private PrintQueue printQueue;
+    private String document = "initial.doc";
+    private Runnable runnable = () -> System.out.println("TEST_END");
+    private PrintThread thread = new PrintThread(document, 10, runnable);
+
+    @Override
+    protected void setUp() {
+        printQueue = new PrintQueue();
+        printQueue.clearQueue();
+        printQueue.push(document, thread);
+    }
+
+    @Test
+    void pushShouldAddNewPrintJobToQueue() {
+        String newDocument = "doc.txt";
+        Runnable newRunnable = () -> System.out.println("SECOND");
+        PrintThread newThread = new PrintThread(newDocument, 10, newRunnable);
+        printQueue.push(newDocument, newThread);
+        assertFalse(printQueue.isEmpty());
+        try {
+            printQueue.get(0).start();
+            Thread.sleep(25);
+            printQueue.get(1).start();
+            Thread.sleep(25);
+            String output = getOutput();
+            assertTrue(output.contains("TEST_END"));
+            assertTrue(output.contains("SECOND"));
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Test
+    void popShouldRemoveFirstPrintJob() {
+        assertFalse(printQueue.isEmpty());
+        printQueue.pop();
+        assertTrue(printQueue.isEmpty());
+    }
+
+    @Test
+    void isEmptyShouldReturnFalseIfQueueHasAtLeastOneElement() {
+        assertFalse(printQueue.isEmpty());
+    }
+
+    @Test
+    void isEmptyShouldReturnTrueIfQueueHasNoElements() {
+        printQueue.pop();
+        assertTrue(printQueue.isEmpty());
+    }
+
+    @Test
+    void getShouldReturnFirstSelectedTaskIndex() {
+        assertEquals(printQueue.get(0), thread);
+    }
+}

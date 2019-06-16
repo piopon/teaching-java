6e149f6455da3229026f1af5137969d8600af229
Piotr Ponikowski <piopon.github@gmail.com>
2019-06-16 15:20:21 +0200

    added builder pattern support enum classes

---
+++ src/pl/smtc/patterns/creational/builder/support/ClosureType.java
@@ -0,0 +1,5 @@
+package pl.smtc.patterns.creational.builder.support;
+
+public enum ClosureType {
+    SCREW, SPORTS, FLIPTOP, CROWN, CORK
+}
---
+++ src/pl/smtc/patterns/creational/builder/support/MaterialType.java
@@ -0,0 +1,5 @@
+package pl.smtc.patterns.creational.builder.support;
+
+public enum MaterialType {
+    PLASTIC, GLASS_WHITE, GLASS_BLUE, GLASS_GREEN, GLASS_BROWN
+}

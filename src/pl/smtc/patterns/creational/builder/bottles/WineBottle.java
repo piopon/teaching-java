556175904d6e4aa9af6d6366d0f27b8234ebe791
Piotr Ponikowski <piopon.github@gmail.com>
2019-06-18 22:50:43 +0200

    another (last one) bottle class: wine bottle

---
+++ src/pl/smtc/patterns/creational/builder/bottles/WineBottle.java
@@ -0,0 +1,28 @@
+package pl.smtc.patterns.creational.builder.bottles;
+
+import pl.smtc.patterns.creational.builder.support.ClosureType;
+import pl.smtc.patterns.creational.builder.support.MaterialType;
+
+public class WineBottle {
+    private final double litreCapacity;
+    private final MaterialType material;
+    private final ClosureType closure;
+    private final String label;
+    private final int puntHeight;
+
+    public WineBottle(double litreCapacity, MaterialType material, ClosureType closure, String label, int puntHeight) {
+        this.litreCapacity = litreCapacity;
+        this.material = material;
+        this.closure = closure;
+        this.label = label;
+        this.puntHeight = puntHeight;
+    }
+
+    @Override
+    public String toString() {
+        String materialStr = material.toString().toLowerCase().replace("_", " ");
+        String closureStr = closure.toString().toLowerCase();
+        String bottleShape = materialStr + " with " + closureStr + " closure and " + puntHeight + "cm punt";
+        return label + " bottle (" + litreCapacity + "l) - " + bottleShape;
+    }
+}

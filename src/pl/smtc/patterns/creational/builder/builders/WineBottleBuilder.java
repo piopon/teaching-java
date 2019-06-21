0ce1a1427ff34ab47538695f0cfe51a32213252e
Piotr Ponikowski <piopon.github@gmail.com>
2019-06-21 17:56:23 +0200

    added second bottle beverage builder: WineBottleBuilder

---
+++ src/pl/smtc/patterns/creational/builder/builders/WineBottleBuilder.java
@@ -0,0 +1,45 @@
+package pl.smtc.patterns.creational.builder.builders;
+
+import pl.smtc.patterns.creational.builder.bottles.WineBottle;
+import pl.smtc.patterns.creational.builder.support.ClosureType;
+import pl.smtc.patterns.creational.builder.support.MaterialType;
+
+public class WineBottleBuilder implements BottleBuilder {
+    private double litreCapacity = 0.0;
+    private MaterialType material;
+    private ClosureType closure;
+    private String label;
+    private int puntHeight = 0;
+
+    @Override
+    public void setCapacity(double litreCapacity) {
+        this.litreCapacity = litreCapacity;
+    }
+
+    @Override
+    public void setMaterial(MaterialType material) {
+        this.material = material;
+    }
+
+    @Override
+    public void setClosure(ClosureType closure) throws IllegalArgumentException {
+        if (ClosureType.SPORTS.equals(closure)) {
+            throw new IllegalArgumentException("Wine bottle cannot have sport closure.");
+        }
+        this.closure = closure;
+    }
+
+    @Override
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public void setPuntHeight(int puntHeight) {
+        this.puntHeight = puntHeight;
+    }
+
+    public WineBottle getResult() {
+        return new WineBottle(litreCapacity, material, closure, label, puntHeight);
+    }
+}

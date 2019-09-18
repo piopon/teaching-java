bb804cc6d68e28e004a29d0a4c4961e0b68454a7
Piotr Ponikowski <piopon.github@gmail.com>
2019-09-18 23:15:00 +0200

    updated unit tests after closure type change

--- test/pl/smtc/patterns/creational/builder/BuilderPatternTest.java
+++ test/pl/smtc/patterns/creational/builder/BuilderPatternTest.java
@@ -20,7 +20,7 @@ class BuilderPatternTest extends BaseTestOut {
         String output = getOutput();
         assertTrue(output.contains("WINE: Chardonnay bottle (0.75l) - glass brown with cork closure and 10cm punt"));
         assertTrue(output.contains("WINE: Sparkling Wine bottle (1.0l) - glass blue with cork closure and 5cm punt"));
-        assertTrue(output.contains("BEVERAGE: Beer bottle (0.5l) - glass green with fliptop closure"));
+        assertTrue(output.contains("BEVERAGE: Beer bottle (0.5l) - glass green with flip top closure"));
         assertTrue(output.contains("BEVERAGE: Coca-Cola bottle (0.33l) - glass white with crown closure"));
         assertTrue(output.contains("BEVERAGE: Soda bottle (0.5l) - plastic with screw closure"));
     }

5522ad06cda902d053e3a4db3925d9c52930583a
Piotr Ponikowski <piopon.github@gmail.com>
2019-10-12 19:50:58 +0200

    added basic examples menu

---
+++ src/pl/smtc/basics/BasicsExamples.java
@@ -0,0 +1,33 @@
+package pl.smtc.basics;
+
+import pl.smtc.basics.hello.HelloWorld;
+import pl.smtc.basics.primitives.PrimitiveTypes;
+import pl.smtc.menu.ConsoleExample;
+import pl.smtc.menu.ConsoleMenu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BasicsExamples implements ConsoleExample {
+    private Map<Integer, ConsoleExample> basicsExamples = new HashMap<>();
+    private int menuWidth = 35;
+
+    @Override
+    public void execute() {
+        addExamples(new HelloWorld(), new PrimitiveTypes());
+
+        ConsoleMenu basicsMenu = new ConsoleMenu(getName(), menuWidth, basicsExamples);
+        basicsMenu.show();
+    }
+
+    @Override
+    public String getName() {
+        return "Basics examples";
+    }
+
+    private void addExamples(ConsoleExample... examples) {
+        for(int i = 0; i < examples.length; i++) {
+            basicsExamples.put(i, examples[i]);
+        }
+    }
+}

98fceb271762631889800c51a6d6741d45e37731
Piotr Ponikowski <piopon.github@gmail.com>
2019-10-12 15:58:12 +0200

    added floating point types example class (not filled yet)

---
+++ src/pl/smtc/basics/primitives/floating/FloatingPointTypes.java
@@ -0,0 +1,15 @@
+package pl.smtc.basics.primitives.floating;
+
+import pl.smtc.menu.ConsoleExample;
+
+public class FloatingPointTypes implements ConsoleExample {
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public String getName() {
+        return "Floating point types";
+    }
+}

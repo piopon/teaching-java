af24a7fac28833c4b23b3f4c85e060f263c1fbfc
Piotr Ponikowski <piopon.github@gmail.com>
2019-10-12 15:58:52 +0200

    new primitive types menu example with floating and integral examples

---
+++ src/pl/smtc/basics/primitives/PrimitiveTypes.java
@@ -0,0 +1,33 @@
+package pl.smtc.basics.primitives;
+
+import pl.smtc.basics.primitives.floating.FloatingPointTypes;
+import pl.smtc.basics.primitives.integral.IntegralTypes;
+import pl.smtc.menu.ConsoleExample;
+import pl.smtc.menu.ConsoleMenu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PrimitiveTypes implements ConsoleExample {
+    private Map<Integer, ConsoleExample> primitivesExamples = new HashMap<>();
+    private int menuWidth = 35;
+
+    @Override
+    public void execute() {
+        addExamples(new IntegralTypes(), new FloatingPointTypes());
+
+        ConsoleMenu primitivesMenu = new ConsoleMenu(getName(), menuWidth, primitivesExamples);
+        primitivesMenu.show();
+    }
+
+    @Override
+    public String getName() {
+        return "Primitive types";
+    }
+
+    private void addExamples(ConsoleExample... examples) {
+        for(int i = 0; i < examples.length; i++) {
+            primitivesExamples.put(i, examples[i]);
+        }
+    }
+}

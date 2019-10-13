aca124c48fa637df28fd20b148e33ed252c3bf5d
Piotr Ponikowski <piopon.github@gmail.com>
2019-10-14 00:21:54 +0200

    added unit tests for primite types menu

---
+++ test/pl/smtc/basics/primitives/PrimitiveTypesTest.java
@@ -0,0 +1,30 @@
+package pl.smtc.basics.primitives;
+
+import org.junit.jupiter.api.Test;
+import pl.smtc.base.BaseTestInOut;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimitiveTypesTest extends BaseTestInOut {
+    private PrimitiveTypes primitiveTypes;
+
+    @Override
+    protected void setUp() {
+        primitiveTypes = new PrimitiveTypes();
+    }
+
+    @Test
+    void executeShouldInvokePrimitiveTypesExamples() {
+        simulateUserInput("0");
+        primitiveTypes.execute();
+        String output = getOutput();
+        assertTrue(output.contains("PRIMITIVE TYPES"));
+        assertTrue(output.contains("1) Integral types"));
+        assertTrue(output.contains("2) Floating point types"));
+    }
+
+    @Test
+    void getNameShouldReturnPrimitiveTypeString() {
+        assertEquals("Primitive types",  primitiveTypes.getName());
+    }
+}

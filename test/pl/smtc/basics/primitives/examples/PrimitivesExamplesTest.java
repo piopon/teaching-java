d3dffc9ede456a1506c6d338bdae9ae902a35723
Piotr Ponikowski <piopon.github@gmail.com>
2019-10-17 23:55:44 +0200

    new unit tests for primitives examples menu

---
+++ test/pl/smtc/basics/primitives/examples/PrimitivesExamplesTest.java
@@ -0,0 +1,30 @@
+package pl.smtc.basics.primitives.examples;
+
+import org.junit.jupiter.api.Test;
+import pl.smtc.base.BaseTestInOut;
+import pl.smtc.base.BaseTestOut;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimitivesExamplesTest extends BaseTestInOut {
+    private PrimitivesExamples primitivesExamples;
+
+    @Override
+    protected void setUp() {
+        primitivesExamples = new PrimitivesExamples();
+    }
+
+    @Test
+    void executeShouldInvokePrimitivesExamplesExample() {
+        simulateUserInput("0");
+        primitivesExamples.execute();
+        String output = getOutput();
+        assertTrue(output.contains("EXAMPLES"));
+        assertTrue(output.contains("1) Weight converter"));
+    }
+
+    @Test
+    void getNameShouldReturnExamplesString() {
+        assertEquals("Examples", primitivesExamples.getName());
+    }
+}

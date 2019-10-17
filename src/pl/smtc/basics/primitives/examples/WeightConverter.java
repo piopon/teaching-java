89c04cd53e7846011e0efd4f753ac826d2e90d83
Piotr Ponikowski <piopon.github@gmail.com>
2019-10-17 21:51:21 +0200

    added weight converter primitive types example

---
+++ src/pl/smtc/basics/primitives/examples/WeightConverter.java
@@ -0,0 +1,25 @@
+package pl.smtc.basics.primitives.examples;
+
+import pl.smtc.menu.ConsoleExample;
+
+import java.util.Scanner;
+
+public class WeightConverter implements ConsoleExample {
+    private final static double POUND_TO_KILOGRAM = 0.45359223;
+
+    @Override
+    public void execute() {
+        System.out.print("Input weight to convert: ");
+        Scanner in = new Scanner(System.in);
+        double input = in.nextDouble();
+        double toKilograms = input * POUND_TO_KILOGRAM;
+        System.out.println(input + "lb = " + toKilograms + "kg");
+        double toPounds = input * 1/POUND_TO_KILOGRAM;
+        System.out.println(input + "kg = " + toPounds + "lb");
+    }
+
+    @Override
+    public String getName() {
+        return "Weight converter";
+    }
+}

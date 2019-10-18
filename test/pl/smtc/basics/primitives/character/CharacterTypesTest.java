c24f76530d0afa6fa6d0ac15d0b600acdc4a6892
Piotr Ponikowski <piopon.github@gmail.com>
2019-10-19 01:41:41 +0200

    new unit tests for primitive character type

---
+++ test/pl/smtc/basics/primitives/character/CharacterTypesTest.java
@@ -0,0 +1,27 @@
+package pl.smtc.basics.primitives.character;
+
+import org.junit.jupiter.api.Test;
+import pl.smtc.base.BaseTestOut;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterTypesTest extends BaseTestOut {
+    private CharacterTypes characterTypes;
+
+    @Override
+    protected void setUp() {
+        characterTypes = new CharacterTypes();
+    }
+
+    @Test
+    void executeShouldInvokeCharacterTypesExample() {
+        characterTypes.execute();
+        String output = getOutput();
+        assertTrue(output.contains("Unicode characters example: P\u0418\u049E \u06E9 2019"));
+    }
+
+    @Test
+    void getNameShouldReturnCharacterTypesString() {
+        assertEquals("Character types", characterTypes.getName());
+    }
+}

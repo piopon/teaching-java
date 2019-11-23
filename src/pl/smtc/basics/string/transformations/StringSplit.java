99d168aadb74b9f8d3ef932e03fd2c3a15602f54
Piotr Ponikowski <piopon.github@gmail.com>
2019-11-23 22:43:32 +0100

    updated split logic with new substring examples

--- src/pl/smtc/basics/string/transformations/StringSplit.java
+++ src/pl/smtc/basics/string/transformations/StringSplit.java
@@ -13,6 +13,7 @@ public class StringSplit implements ConsoleExample {
         System.out.println("Input string: " + INPUT_STRING);
         System.out.println("---------------------------------");
         splitExamples();
+        substringExamples();
     }
 
     @Override
@@ -25,5 +26,19 @@ public class StringSplit implements ConsoleExample {
         System.out.println("All split: " + splitAll);
         List<String> splitLimit = Arrays.asList(INPUT_STRING.split("\\s", 3));
         System.out.println("Limit split: " + splitLimit);
+    }
+
+    private void substringExamples() {
+        final int lowBound = 6, topBound = 13, offBound = 100;
+        try {
+            String substringOneBound = INPUT_STRING.substring(lowBound);
+            System.out.println("Substring one bound: " + substringOneBound);
+            String substringTwoBound = INPUT_STRING.substring(lowBound, topBound);
+            System.out.println("Substring two bounds: " + substringTwoBound);
+            String substringOffBound = INPUT_STRING.substring(lowBound, offBound);
+            System.out.println("Substring off bounds: " + substringOffBound);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Substring error: " + e.getMessage());
+        }
     }
 }

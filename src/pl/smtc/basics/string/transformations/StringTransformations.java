f81e41431b147bed589d2170f80c79332c02355b
Piotr Ponikowski <piopon.github@gmail.com>
2019-11-23 21:53:59 +0100

    added string transformations menu with string replace example

---
+++ src/pl/smtc/basics/string/transformations/StringTransformations.java
@@ -0,0 +1,31 @@
+package pl.smtc.basics.string.transformations;
+
+import pl.smtc.menu.ConsoleExample;
+import pl.smtc.menu.ConsoleMenu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringTransformations implements ConsoleExample {
+    private Map<Integer, ConsoleExample> stringTransformations = new HashMap<>();
+    private int menuWidth = 35;
+
+    @Override
+    public void execute() {
+        addExamples(new StringReplace());
+
+        ConsoleMenu stringMenu = new ConsoleMenu(getName(), menuWidth, stringTransformations);
+        stringMenu.show();
+    }
+
+    @Override
+    public String getName() {
+        return "String transformations";
+    }
+
+    private void addExamples(ConsoleExample... examples) {
+        for(int i = 0; i < examples.length; i++) {
+            stringTransformations.put(i, examples[i]);
+        }
+    }
+}

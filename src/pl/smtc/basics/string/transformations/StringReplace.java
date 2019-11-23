f59ce78b825d886ca73c90e0b879892c2f767b87
Piotr Ponikowski <piopon.github@gmail.com>
2019-11-23 21:51:44 +0100

    added string replace example logic

---
+++ src/pl/smtc/basics/string/transformations/StringReplace.java
@@ -0,0 +1,25 @@
+package pl.smtc.basics.string.transformations;
+
+import pl.smtc.menu.ConsoleExample;
+
+public class StringReplace implements ConsoleExample {
+    @Override
+    public void execute() {
+        String inputString = "This is a test string";
+        System.out.println("Input string: " + inputString);
+        System.out.println("---------------------------------");
+        String charReplace = inputString.replace(' ', '_');
+        System.out.println("Char replace: " + charReplace);
+        String stringReplace = inputString.replace("test", "final");
+        System.out.println("String replace: " + stringReplace);
+        String firstReplace = inputString.replaceFirst("\\s", "");
+        System.out.println("First replace: " + firstReplace);
+        String allReplace = inputString.replaceAll("\\s", "");
+        System.out.println("All replace: " + allReplace);
+    }
+
+    @Override
+    public String getName() {
+        return "String replace";
+    }
+}

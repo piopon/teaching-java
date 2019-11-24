b40ba94985559c3d73304d4fdc89a0430cda3666
Piotr Ponikowski <piopon.github@gmail.com>
2019-11-24 18:03:03 +0100

    added basic string transformation examples

---
+++ src/pl/smtc/basics/string/transformations/StringBasic.java
@@ -0,0 +1,26 @@
+package pl.smtc.basics.string.transformations;
+
+import pl.smtc.menu.ConsoleExample;
+
+public class StringBasic implements ConsoleExample {
+    private final static String INPUT_STRING = "  This is a test string";
+
+    @Override
+    public void execute() {
+        String added = INPUT_STRING + " with const addition.";
+        System.out.println("Added string: " + added);
+        String lower = INPUT_STRING.toLowerCase();
+        System.out.println("Lower string: " + lower);
+        String upper = INPUT_STRING.toUpperCase();
+        System.out.println("Upped string: " + upper);
+        String trimmed = INPUT_STRING.trim();
+        System.out.println("Trimmed string: " + trimmed);
+        String joined = String.join("+", "my", "test", "str");
+        System.out.println("Joined string: " + joined);
+    }
+
+    @Override
+    public String getName() {
+        return "String basic transformations";
+    }
+}

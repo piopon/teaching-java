69c84e3b8d320452e657e7649410bebdb32569ee
Piotr Ponikowski <piopon.github@gmail.com>
2019-11-28 23:07:26 +0100

    new string checks examples menu

---
+++ src/pl/smtc/basics/string/checks/StringChecks.java
@@ -0,0 +1,31 @@
+package pl.smtc.basics.string.checks;
+
+import pl.smtc.menu.ConsoleExample;
+import pl.smtc.menu.ConsoleMenu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StringChecks implements ConsoleExample {
+    private Map<Integer, ConsoleExample> stringChecks = new HashMap<>();
+    private int menuWidth = 35;
+
+    @Override
+    public void execute() {
+        addExamples(new StringBounds());
+
+        ConsoleMenu checksMenu = new ConsoleMenu(getName(), menuWidth, stringChecks);
+        checksMenu.show();
+    }
+
+    @Override
+    public String getName() {
+        return "String checks";
+    }
+
+    private void addExamples(ConsoleExample... examples) {
+        for(int i = 0; i < examples.length; i++) {
+            stringChecks.put(i, examples[i]);
+        }
+    }
+}

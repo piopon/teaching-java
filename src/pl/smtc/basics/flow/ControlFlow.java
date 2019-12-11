49f986ca4449b5bd3e258526823f17135c5aea2f
Piotr Ponikowski <piopon.github@gmail.com>
2019-12-11 17:50:09 +0100

    added control flow menu with if-else example

---
+++ src/pl/smtc/basics/flow/ControlFlow.java
@@ -0,0 +1,32 @@
+package pl.smtc.basics.flow;
+
+import pl.smtc.basics.flow.decision.IfElseifElseExample;
+import pl.smtc.menu.ConsoleExample;
+import pl.smtc.menu.ConsoleMenu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControlFlow implements ConsoleExample {
+    private Map<Integer, ConsoleExample> controlFlowExamples = new HashMap<>();
+    private int menuWidth = 35;
+
+    @Override
+    public void execute() {
+        addExamples(new IfElseifElseExample());
+
+        ConsoleMenu controlFlowMenu = new ConsoleMenu(getName(), menuWidth, controlFlowExamples);
+        controlFlowMenu.show();
+    }
+
+    @Override
+    public String getName() {
+        return "Control flow";
+    }
+
+    private void addExamples(ConsoleExample... examples) {
+        for(int i = 0; i < examples.length; i++) {
+            controlFlowExamples.put(i, examples[i]);
+        }
+    }
+}

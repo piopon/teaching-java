ca0e00ebec5f7f9fd3539d397b2eb44fdf8bea52
Piotr Ponikowski <piopon.github@gmail.com>
2019-12-22 15:10:50 +0100

    added examples menu

---
+++ src/pl/smtc/basics/flow/examples/FlowExamples.java
@@ -0,0 +1,31 @@
+package pl.smtc.basics.flow.examples;
+
+import pl.smtc.menu.ConsoleExample;
+import pl.smtc.menu.ConsoleMenu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlowExamples implements ConsoleExample {
+    private Map<Integer, ConsoleExample> flowExamples = new HashMap<>();
+    private int menuWidth = 35;
+
+    @Override
+    public void execute() {
+        addExamples(new SumOddNumbers());
+
+        ConsoleMenu examplesMenu = new ConsoleMenu(getName(), menuWidth, flowExamples);
+        examplesMenu.show();
+    }
+
+    @Override
+    public String getName() {
+        return "Examples";
+    }
+
+    private void addExamples(ConsoleExample... examples) {
+        for(int i = 0; i < examples.length; i++) {
+            flowExamples.put(i, examples[i]);
+        }
+    }
+}

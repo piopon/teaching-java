e06bb039eeffa8f3a37e49f8adfcea4b870af7a9
Piotr Ponikowski <piopon.github@gmail.com>
2020-01-01 21:31:37 +0100

    fixed unit tests for days in month example

--- test/pl/smtc/basics/flow/examples/DaysInMonthTest.java
+++ test/pl/smtc/basics/flow/examples/DaysInMonthTest.java
@@ -1,7 +1,12 @@
 package pl.smtc.basics.flow.examples;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import pl.smtc.base.BaseTestInOut;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,7 +23,7 @@ class DaysInMonthTest extends BaseTestInOut {
         simulateUserInput("12/2018");
         daysInMonth.execute();
         String output = getOutput();
-        assertTrue(output.contains("First and last digits sum = 7"));
+        assertTrue(output.contains("Days in 12/2018: 31"));
     }
 
     @Test
@@ -27,7 +32,40 @@ class DaysInMonthTest extends BaseTestInOut {
     }
 
     @Test
-    void getDaysInMonth() {
+    void getDaysInMonthShouldThrowOnBadMonth() {
+        assertThrows(NumberFormatException.class, () -> daysInMonth.getDaysInMonth(-1,1000));
+        assertThrows(NumberFormatException.class, () -> daysInMonth.getDaysInMonth(15,1000));
+    }
 
+    @Test
+    void getDaysInMonthShouldThrowOnBadYear() {
+        assertThrows(NumberFormatException.class, () -> daysInMonth.getDaysInMonth(1,-1000));
+        assertThrows(NumberFormatException.class, () -> daysInMonth.getDaysInMonth(1,10000));
+    }
+
+    @ParameterizedTest(name = "Month {0}/{1} = {2} days")
+    @MethodSource("daysInMonthTestData")
+    void getDaysInMonthShouldReturnCorrectOutputOnGoodInput(int month, int year, int days) {
+        assertEquals(days, daysInMonth.getDaysInMonth(month, year));
+    }
+
+    private static Stream<Arguments> daysInMonthTestData() {
+        return Stream.of(
+                Arguments.of(1, 1000, 31),
+                Arguments.of(4, 1200, 30),
+                Arguments.of(7, 1613, 31),
+                Arguments.of(4, 1914, 30),
+                Arguments.of(2, 1950, 28),
+                Arguments.of(10, 1987, 31),
+                Arguments.of(6, 1991, 30),
+                Arguments.of(5, 1999, 31),
+                Arguments.of(11, 2001, 30),
+                Arguments.of(3, 2012, 31),
+                Arguments.of(9, 2015, 30),
+                Arguments.of(2, 2016, 29),
+                Arguments.of(2, 2019, 28),
+                Arguments.of(7, 2019, 31),
+                Arguments.of(12, 2019, 31),
+                Arguments.of(2, 2020, 29));
     }
 }

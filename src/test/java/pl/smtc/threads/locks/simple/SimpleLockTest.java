6e0acc03f2957904ae46fdc6c52ceb2d6ace0098
Piotr Ponikowski <piopon.github@gmail.com>
2020-01-15 17:17:30 +0100

    updated simple lock test: check string with thread 1 OR 2

--- src/test/java/pl/smtc/threads/locks/simple/SimpleLockTest.java
+++ src/test/java/pl/smtc/threads/locks/simple/SimpleLockTest.java
@@ -3,6 +3,8 @@ package pl.smtc.threads.locks.simple;
 import org.junit.jupiter.api.Test;
 import pl.smtc.base.BaseTestOut;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleLockTest extends BaseTestOut {
@@ -17,14 +19,21 @@ class SimpleLockTest extends BaseTestOut {
     void executeShouldInvokeSimpleLockImplementation() {
         simpleLock.execute();
         String output = getOutput();
-        assertTrue(output.contains("Thread 1 start value: 3"));
+        System.out.println("Test output: " + output);
+        assertTrue(outputContainsOneOf(output,
+                "Thread 1 start value: 3", "Thread 2 start value: 3"));
         assertTrue(output.contains("+--> Countdown: 2  1  0"));
-        assertTrue(output.contains("Thread 2 start value: 0"));
+        assertTrue(outputContainsOneOf(output,
+                "Thread 1 start value: 0", "Thread 2 start value: 0"));
         assertTrue(output.contains("+--> Countdown: -1  -2  -3"));
     }
 
     @Test
     void getNameShouldReturnSimpleLockImplementationString() {
         assertEquals("Simple lock implementation", simpleLock.getName());
+    }
+
+    private boolean outputContainsOneOf(String output, String ... options) {
+        return Arrays.stream(options).filter(output::contains).count() == 1;
     }
 }
